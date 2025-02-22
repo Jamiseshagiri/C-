d26eaebce43c707e6ec6d73a403d93500a6bd853
giri <jamisehagiri78@gmail.com>
2025-02-22 20:28:27 +0530

    Frist commit

---
+++ addtwonum.cs
@@ -0,0 +1,10 @@
+using System;
+class addtwonum{
+ static void Main() {
+        float a=10;
+        float b=20;
+        float c=a+b;
+        Console.WriteLine("add two numbers is "+c);
+    }//main
+    
+}
---
+++ patter1.cs
@@ -0,0 +1,10 @@
+using System;
+class patter1{
+    public static void Main(string[] args){
+            for(int i=0;i<=4;i++){
+               Console.WriteLine(""+i);
+            }
+       
+    }
+
+}
